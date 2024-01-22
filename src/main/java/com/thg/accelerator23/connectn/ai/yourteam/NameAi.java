387e46fc8f864895e0c444a6e46a296c3ea29308
Victor Elgersma <victor.elgersma41@thehutgroup.com>
2024-01-22 13:45:25 +0000

    Revert "updated POM and added random AI player"
    This reverts commit bd1dfa0656fda88e4126bdb0026f481f699b14e4.

--- src/main/java/com/thg/accelerator23/connectn/ai/yourteam/NameAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/yourteam/NameAi.java
@@ -1,26 +1,20 @@
-package com.thg.accelerator23.connectn.ai.makejarnotwar;
+package com.thg.accelerator23.connectn.ai.yourteam;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class MakeJarNotWarConnectFour_v1 extends Player {
-  public MakeJarNotWarConnectFour_v1(Counter counter) {
+public class NameAi extends Player {
+  public NameAi(Counter counter) {
     //TODO: fill in your name here
-    super(counter, MakeJarNotWarConnectFour_v1.class.getName());
+    super(counter, NameAi.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
-    int width = board.getConfig().getWidth();
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-
-    return getRandomNumber(0,width);
-  }
-
-  private int getRandomNumber(int min, int max) {
-    return (int) ((Math.random() * (max - min)) + min);
+    return 4;
   }
 }
